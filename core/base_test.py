961183c1422bd580d8b70641ba58da20ab7070ac
Artem Aslyamov <aaslyamov.arts@gmail.com>
2025-04-17 16:19:43 +0300

    feat: Adds helper methods for page interaction and element retrieval.

--- core/base_test.py
+++ core/base_test.py
@@ -1,6 +1,8 @@
-
 from core.web_driver import driver as dr
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from config import settings
 
 
 class BaseTest:
@@ -11,3 +13,29 @@ class BaseTest:
     def teardown_method(self):
         self.driver.quit()
 
+    def open_page(self, url=settings.default.url):
+        self.driver.get(url)
+
+    def click_and_wait_for_url(
+        self, by, locator, url_contains, timeout=settings.default.timeout
+    ):
+        wait = WebDriverWait(self.driver, timeout)
+        try:
+            element = wait.until(EC.element_to_be_clickable((by, locator)))
+            element.click()
+            wait.until(EC.url_contains(url_contains))
+            return True
+        except TimeoutException:
+            raise TimeoutException(
+                f"The element '{locator}' is not clickable or URL does not contain '{url_contains}' for {timeout} seconds."
+            )
+
+    def get_elements(self, by, locator, timeout=settings.default.timeout):
+        wait = WebDriverWait(self.driver, timeout)
+        try:
+            elements = wait.until(EC.presence_of_all_elements_located((by, locator)))
+            return [elem.text.strip() for elem in elements if elem.text.strip()]
+        except TimeoutException:
+            raise TimeoutException(
+                f"Elements according to the locator ({by}, '{locator}') were not found for {timeout} seconds."
+            )
