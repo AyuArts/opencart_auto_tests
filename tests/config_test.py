8a29e47b496cf5d9ce70eee4af3738c16c269e70
Artem Aslyamov <aaslyamov.arts@gmail.com>
2025-04-17 19:45:26 +0300

    feat: add 'One' model to enhance config testing utilities

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Dict
+
+
+from core.random_user import user
 
 
 class Zero(BaseModel):
@@ -15,5 +18,26 @@ class Zero(BaseModel):
     css_selector: str = ".col-sm-3 a"
 
 
+class One(BaseModel):
+    dropdown: str = '//*[@id="top-links"]/ul/li[2]/a/span[2]'
+    element_reg: str = '//*[@id="top-links"]/ul/li[2]/ul/li[1]/a'
+    url_reg: str = "route=account/register"
+    fields_regs: Dict[str, str] = {
+        "input-firstname": user.first_name,
+        "input-lastname": user.last_name,
+        "input-email": user.email,
+        "input-telephone": user.phone,
+        "input-password": user.password,
+        "input-confirm": user.password,
+    }
+
+    check_box: str = '//*[@id="content"]/form/div/div/input[1]'
+    button_continue: str = '//*[@id="content"]/form/div/div/input[2]'
+    url_approve_reg: str = "route=account/success"
+    text_correct: str = "h1"
+    text_expected: str = "Your Account Has Been Created!"
+
+
 class Numbers(BaseModel):
     zero: Zero = Field(default_factory=Zero)
+    one: One = Field(default_factory=One)
