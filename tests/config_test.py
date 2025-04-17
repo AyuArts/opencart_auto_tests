1f63e72e8fe0359373635a7058308a988de75edf
Artem Aslyamov <aaslyamov.arts@gmail.com>
2025-04-17 16:19:57 +0300

    feat: Adds config for zero page and associated elements.

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,5 +1,19 @@
 from pydantic import BaseModel, Field
+from typing import List
+
+
+class Zero(BaseModel):
+    url_contains: str = "route=product/manufacturer"
+    expected_brands: List[str] = [
+        "Apple",
+        "Canon",
+        "Hewlett-Packard",
+        "HTC",
+        "Palm",
+        "Sony",
+    ]
+    css_selector: str = ".col-sm-3 a"
 
 
 class Numbers(BaseModel):
-    pass
+    zero: Zero = Field(default_factory=Zero)
