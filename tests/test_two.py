6e3e713b14882a4902f458da1faa0104783c07f2
Artem Aslyamov <aaslyamov.arts@gmail.com>
2025-04-18 15:53:27 +0300

    feat: Implement test_two method for enhanced test coverage

--- tests/test_two.py
+++ tests/test_two.py
@@ -5,4 +5,45 @@ from selenium.webdriver.common.by import By
 
 
 class TestTwo(BaseTest):
-    pass
+    def test_two(self):
+        self.open_page()
+
+        self.cursor_guidance(
+            By.LINK_TEXT,
+            settings.tests.two.dropdown,
+        )
+
+        self.click(
+            By.LINK_TEXT,
+            settings.tests.two.button_text,
+        )
+        assert (
+            self.first_selected(By.ID, settings.tests.two.input_limit_id).text
+            == settings.tests.two.input_limit_value_def
+        )
+        assert (
+            self.first_selected(By.ID, settings.tests.two.input_sort_id).text
+            == settings.tests.two.input_sort_value_def
+        )
+        assert (
+            len(self.find_elements(By.CLASS_NAME, settings.tests.two.product_layout))
+            == settings.tests.two.product_count
+        )
+
+        self.select_by_visible_text(
+            By.ID,
+            settings.tests.two.input_limit_id,
+            settings.tests.two.input_limit_value_new,
+        )
+        assert (
+            self.first_selected(By.ID, settings.tests.two.input_limit_id).text
+            == settings.tests.two.input_limit_value_new
+        )
+
+        pagination = self.find_element(
+            By.CSS_SELECTOR, settings.tests.two.pagination_selector
+        )
+        assert pagination.text == settings.tests.two.pagination_text
+
+        self.execute_script(settings.tests.two.script_scroll)
+        assert pagination.is_displayed()
