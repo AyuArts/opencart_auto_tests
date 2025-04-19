32141f04b9376223ab0f94bf062438ad45ba93c2
Artem Aslyamov <aaslyamov.arts@gmail.com>
2025-04-19 14:24:45 +0300

    tests: create steps for homepage actions and product sorting checks.

--- tests/test_three.py
+++ tests/test_three.py
@@ -4,6 +4,7 @@ from core.base_test import BaseTest
 from config import settings
 from selenium.webdriver.common.by import By
 from core.logger import Logger
+from core.utils import is_sorted_ascending
 
 log = Logger().get_logger("Test_three")
 
@@ -33,5 +34,54 @@ class TestThree(BaseTest):
         6. Select 'Price (Low > High)' from Sort by dropdown
         7. Check that products were sorted correctly
         """
+        with allure.step("Step 1: Open the homepage"):
+            log.info("Opening the homepage.")
+            self.open_page()
 
-        pass
+        with allure.step("Step 2: Hover over the dropdown menu"):
+            log.info(f"Hovering over dropdown: {settings.tests.two.dropdown}")
+            self.cursor_guidance(
+                By.LINK_TEXT,
+                settings.tests.two.dropdown,
+            )
+
+        with allure.step("Step 3: Click on the catalog section"):
+            log.info(f"Clicking button: {settings.tests.two.button_text}")
+            self.click(
+                By.LINK_TEXT,
+                settings.tests.two.button_text,
+            )
+
+        with allure.step("Step 4: We get the names of products'"):
+            filter_sort = "Name (A - Z)"
+            log.info(f"Changing 'Sort' to: {filter_sort}")
+            self.select_by_visible_text(
+                By.ID, settings.tests.two.input_sort_id, filter_sort
+            )
+            elements = self.find_elements(By.TAG_NAME, "h4")
+            products = [el.text.lower() for el in elements]
+            log.info(f"Product names found: {products}")
+
+        with allure.step(
+            "Step 5: Verification of product sorting filter: 'Name (A - Z)'"
+        ):
+            assert is_sorted_ascending(products), "Products are not sorted A-Z"
+
+        with allure.step("Step 6: We get the prices of products"):
+            filter_sort = "Price (Low > High)"
+            log.info(f"Changing 'Sort' to: {filter_sort}")
+            self.select_by_visible_text(
+                By.ID, settings.tests.two.input_sort_id, filter_sort
+            )
+            elements = self.find_elements(By.CLASS_NAME, "price-new")
+            products_price = [
+                float(el.text.replace("$", "").strip()) for el in elements
+            ]
+            log.info(f"Product price found: {products_price}")
+
+        with allure.step(
+            "Step 7: Verification of product sorting filter: 'Price (Low > High)'"
+        ):
+            assert is_sorted_ascending(
+                products_price
+            ), "Products are not sorted Low > High"
